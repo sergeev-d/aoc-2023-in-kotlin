99164309f78a3b9ef73af920fe5e0ffd3e1ec6f0
Denis Sergeev <d.sergeev@thesollution.com>
2023-12-09 21:51:37 +0300

    feat (solution): day 9, more elegant implementation

--- src/Day09.kt
+++ src/Day09.kt
@@ -17,24 +17,31 @@ fun main() {
         }
     }
 
+    fun findNextValue2(values: List<Int>): Sequence<List<Int>> {
+        return generateSequence(values) { currentValues ->
+            if (currentValues.any { it != 0 }) {
+                currentValues.windowed(2) { (v1, v2) -> v2 - v1 }
+            } else {
+                null
+            }
+        }
+    }
+
     fun part1(input: List<String>): Int {
         return input.sumOf { s ->
-            findNextValue(s.split("\\s".toRegex()).map { it.toInt() }, isLast = true)
+            findNextValue2(s.split("\\s".toRegex()).map { it.toInt() }).sumOf { it.last() }
         }
     }
 
 
     fun part2(input: List<String>): Int {
         return input.sumOf { s ->
-            findNextValue(s.split("\\s".toRegex()).map { it.toInt() }, isLast = false)
+            findNextValue2(s.split("\\s".toRegex()).map { it.toInt() }).map { v -> v.first() }.toList()
+                .reduceRight(Int::minus)
         }
     }
 
     val input = readInput(CURRENT_DAY)
     part1(input).println()
     part2(input).println()
-}
-
-private fun process(): Int {
-    return 1
 }
